b85cd8ec13ba67996bc1aded5e801ca8df3fbd49
EmilioArriola <emilio280905@gmai.com>
2022-08-24 08:38:24 -0600

    suma modo de consola 24082022

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    println("aplicacion Emilio Arriola")
+    println("ingrese un numero 1: ")
+    val n1 = readLine()!!.toInt()
+
+    println("ingrese un numero 2: ")
+    val n2 = readLine()!!.toInt()
+
+    val resultado = n1 + n2
+    println("El resultado es: " +resultado)
+
+
+
+
+}
